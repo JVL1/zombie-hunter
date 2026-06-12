aa3eda1b272aa17d96ed46f4c22895c896afb95d
JVL1 <jvanlente.e3@gmail.com>
2026-06-12 07:21:16 -0700

    refactor: extract common + city texture packs to src/art

--- src/art/art.test.ts
+++ src/art/art.test.ts
@@ -1,9 +1,21 @@
 import { describe, expect, it } from 'vitest';
+import * as city from './city';
+import * as common from './common';
 import * as helpers from './helpers';
 
 describe('art module exports', () => {
   it('helpers exports bakeTint and bakeSheet functions', () => {
     expect(typeof helpers.bakeTint).toBe('function');
     expect(typeof helpers.bakeSheet).toBe('function');
+  });
+
+  it('common exports generateCommonTextures function', () => {
+    expect(Object.keys(common)).toEqual(['generateCommonTextures']);
+    expect(typeof common.generateCommonTextures).toBe('function');
+  });
+
+  it('city exports generateCityTextures function', () => {
+    expect(Object.keys(city)).toEqual(['generateCityTextures']);
+    expect(typeof city.generateCityTextures).toBe('function');
   });
 });
